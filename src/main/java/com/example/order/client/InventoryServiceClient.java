8c5b250444186367c19ccbe13bf30045754e6884
4631 <alessio.zubiani@popso.it>
2024-10-31 12:38:40 +0100

    Aggiunto colloquio con servizio InventoryService tramite REST API

---
+++ src/main/java/com/example/order/client/InventoryServiceClient.java
@@ -0,0 +1,14 @@
+package com.example.order.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "inventory-service", url = "http://localhost:8082")
+public interface InventoryServiceClient {
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+	boolean isInStock(@RequestParam("skuCode") String skuCode, @RequestParam("quantity") Integer quantity);
+
+}
--- src/main/java/com/example/order/service/OrderServiceImpl.java
+++ src/main/java/com/example/order/service/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.order.service;
 
 import java.util.UUID;
 
+import com.example.order.client.InventoryServiceClient;
 import com.example.order.dto.OrderRequest;
 import com.example.order.model.Order;
 import com.example.order.repository.OrderRepository;
@@ -15,19 +16,25 @@ import lombok.RequiredArgsConstructor;
 public class OrderServiceImpl implements OrderService {
 
 	private final OrderRepository orderRepository;
+	private final InventoryServiceClient inventoryServiceClient;
 	
 	
 	@Override
 	public void placeOrder(OrderRequest orderRequest) {
 		
-		Order order = Order.builder()
-				.orderNumber(UUID.randomUUID().toString())
-				.price(orderRequest.price())
-				.skuCode(orderRequest.skuCode())
-				.quantity(orderRequest.quantity())
-				.build();
-		
-		this.orderRepository.save(order);
+		boolean isInStock = this.inventoryServiceClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+		if(isInStock) {
+			Order order = Order.builder()
+					.orderNumber(UUID.randomUUID().toString())
+					.price(orderRequest.price())
+					.skuCode(orderRequest.skuCode())
+					.quantity(orderRequest.quantity())
+					.build();
+			
+			this.orderRepository.save(order);
+		} else {
+			throw new RuntimeException("Product with SkuCode [".concat(orderRequest.skuCode()).concat("] is not in stock"));
+		}
 	}
 
 }
